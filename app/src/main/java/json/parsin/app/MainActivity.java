ce0c55e98fd61ebebb39df7082a77b730d04f94d
Dominik277 <dominikdoric277@gmail.com>
2020-12-08 20:17:31 +0100

    Prvi commit.

---
+++ app/src/main/java/json/parsin/app/MainActivity.java
@@ -0,0 +1,14 @@
+package json.parsin.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
