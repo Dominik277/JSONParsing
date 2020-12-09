097b3fb80ba70b25c9624b03bbd958d9aaa7058a
Dominik277 <dominikdoric277@gmail.com>
2020-12-09 14:21:05 +0100

    Dodane reference preko kojih cemo pozivati neke objekte ili varijable.

--- app/src/main/java/json/parsin/app/CustomAdapter.java
+++ app/src/main/java/json/parsin/app/CustomAdapter.java
@@ -1,12 +1,30 @@
 package json.parsin.app;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    ArrayList<String> personNames;
+    ArrayList<String> emailIds;
+    ArrayList<String> mobileNumbers;
+    Context context;
+
+    public CustomAdapter(Context context, ArrayList<String> personNames, ArrayList<String> emailIds, ArrayList<String> mobileNumbers) {
+        this.context = context;
+        this.personNames = personNames;
+        this.emailIds = emailIds;
+        this.mobileNumbers = mobileNumbers;
+    }
+
+
     @NonNull
     @Override
     public CustomAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -24,8 +42,15 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name,email,mobileNo;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            name = itemView.findViewById(R.id.name);
+            email = itemView.findViewById(R.id.email);
+            mobileNo = itemView.findViewById(R.id.mobileNo);
         }
     }
 
