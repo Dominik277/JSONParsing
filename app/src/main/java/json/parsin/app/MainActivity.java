3c199f7679e1ebac5b0e53dcebbb3881a12f9010
Dominik277 <dominikdoric277@gmail.com>
2020-12-09 09:11:31 +0100

    Dodana metoda loadJSONFromAssets().

--- app/src/main/java/json/parsin/app/MainActivity.java
+++ app/src/main/java/json/parsin/app/MainActivity.java
@@ -1,14 +1,46 @@
 package json.parsin.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> personNames = new ArrayList<>();
+    ArrayList<String> emailIds = new ArrayList<>();
+    ArrayList<String> mobileNumbers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
     }
+
+    public String loadJSONFromAsset(){
+        String json = null;
+        try {
+            InputStream is = getAssets().open("users_list.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer,"UTF-8");
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
 }
