7bb54fdf1c8b69a9c2ca3b16d95ba65cfcff2acb
Dominik277 <dominikdoric277@gmail.com>
2020-12-12 22:26:17 +0100

    Dio metode doInBackground() implementiran.

--- app/src/main/java/json/parsin/app/MainActivity.java
+++ app/src/main/java/json/parsin/app/MainActivity.java
@@ -3,10 +3,14 @@ package json.parsin.app;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -69,6 +73,39 @@ public class MainActivity extends AppCompatActivity {
         //ne bi smjele dodirivati nikakve activitie i fragmente od main thread-a.
         @Override
         protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            String url = "http://api.androidhive.info/contacts/";
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG,"Response from url: " + jsonStr);
+
+            if (jsonStr != null){
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    JSONArray contacts = jsonObj.getJSONArray("contacts");
+
+                    for (int i = 0;i<contacts.length();i++){
+                        JSONObject c = contacts.getJSONObject(i);
+                        String id = c.getString("id");
+                        String name = c.getString("name");
+                        String email = c.getString("email");
+                        String address = c.getString("address");
+                        String gender = c.getString("gender");
+
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");
+
+                        HashMap<String,String> contact = new HashMap<>();
+
+
+                    }
+                }catch (){
+
+                }
+            }
+
             return null;
         }
 
