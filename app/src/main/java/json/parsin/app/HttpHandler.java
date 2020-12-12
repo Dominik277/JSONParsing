79ea889c70f59e054de63b9bf0ccbf40307a2858
Dominik277 <dominikdoric277@gmail.com>
2020-12-12 21:58:28 +0100

    Dodana convertStreamToString() metoda.

--- app/src/main/java/json/parsin/app/HttpHandler.java
+++ app/src/main/java/json/parsin/app/HttpHandler.java
@@ -3,8 +3,10 @@ package json.parsin.app;
 import android.util.Log;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
@@ -38,6 +40,28 @@ public class HttpHandler {
             Log.e(TAG,"Exception: " + e.getMessage());
         }
         return response;
+    }
+
+    private String convertStreamToString(InputStream is){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+
+        try {
+            while ((line = reader.readLine())!= null){
+                sb.append(line).append("\n");
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                is.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 
 }
